76fcb29e28af167d507cc31472aecec43dd55596
Fraol-X <pfresh641@gmail.com>
2024-07-12 18:15:29 +0300

    Implement a Simple Interactive Quiz

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    let feedBack = document.getElementById("feedback");
+    if (userAnswer === correctAnswer) {
+        feedBack.textContent = "Correct! Well done."
+    } else {
+        feedBack.textContent = "That's incorrect. Try again!"
+    }
+}
+
+const submitBtn = document.getElementById("submit-answer");
+submitBtn.addEventListener('click' , checkAnswer);
+
+
